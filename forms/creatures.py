513108ba6d7fc3d529505b467906fc26bc0db1b6
Marshall G. Gates <mggates@gmail.com>
2026-03-17 17:45:33 -0400

    Improved special abilities layout

--- forms/creatures.py
+++ forms/creatures.py
@@ -613,7 +613,7 @@ class CreatureForm:
             self.special_abilities_entry.delete("1.0", END)
             self.special_abilities_entry['height'] = len(self.creature.special_abilities) * 3
             for special_ability in self.creature.special_abilities:
-                self.special_abilities_entry.insert(END, getattr(special_ability, 'ability') + " " + getattr(special_ability, 'type') + "\n")
+                self.special_abilities_entry.insert(END, getattr(special_ability, 'ability') + " " + getattr(special_ability, 'type') + "\n", "bold_tag")
                 self.special_abilities_entry.insert(END, getattr(special_ability, 'description') + "\n\n")
 
             self.special_gear_entry.delete("1.0", END)
