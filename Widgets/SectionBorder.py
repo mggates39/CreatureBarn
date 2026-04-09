59240cdfa136c7df27e228d118af5e916b119efd
Marshall G. Gates <mggates@gmail.com>
2026-04-08 23:58:37 -0400

    Make properly customtkinter compatible

--- Widgets/SectionBorder.py
+++ Widgets/SectionBorder.py
@@ -1,13 +1,12 @@
-import tkinter as tk
-from tkinter import ttk
+import customtkinter
 
 
-class SectionBorder(ttk.Frame):
+class SectionBorder(customtkinter.CTkFrame):
     def __init__(self, container, title, *args, **kwargs):
         super().__init__(container, *args, **kwargs)
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
 
-        self.label = ttk.Label(self, text=title)
+        self.label = customtkinter.CTkLabel(self, text=title)
         self.label.configure(font=('Helvetica', 16, 'bold'))
-        self.label.grid(row=0, column=0)
+        self.label.grid(row=0, column=0, sticky='ew')
